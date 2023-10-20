272f468aa3cff3f2492fbacd81ef2ac08f8de380
VicJo <VicJohnathanWilson@outlook.com>
2023-10-20 16:26:34 +0800

    added and implemented a basic auth python file.

--- api/main.py
+++ api/main.py
@@ -1,6 +1,13 @@
+import casbin
 from fastapi import FastAPI
 
+
 api = FastAPI()
+
+enforcer = casbin.Enforcer(
+    'rbac_model.conf',
+    'rbac_policy.csv',
+)
 
 @api.get('/')
 async def index():
